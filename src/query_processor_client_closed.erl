9d9a05bdc00a98aa76f151ea920e10d18f02d9a4
NOMORECOFFEE <github.coffee@hotmail.com>
2014-05-05 16:40:08 +0400

    Update query_processor_client_closed.erl

--- src/query_processor_client_closed.erl
+++ src/query_processor_client_closed.erl
@@ -1,3 +1,19 @@
 -module(query_processor_client_closed).
 
 -behaviour(gen_event).
+
+-export([
+    init/1,
+    handle_event/2,
+    terminate/2
+]).
+    
+handle_event({request, Packet}, #state{id = Id} = StateIn) ->
+    gen_event:notify(self(), {request, Packet}),
+    {swap_handler, normal, StateIn, query_processor_client_connected, Id}.
+
+handle_call(_Event, StateIn) ->
+    {ok, {error, _Event}, StateIn}.
+    
+terminate(_, StateIn) ->
+    ok
